99fa034f8c5d3bdf264a87349482bd0b384f5b5b
John Westwood <johnjai@sbcglobal.net>
2013-07-07 21:45:51 +0000

    Added index.html and modified web.js to display its contents

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+// var buf = require('buf');
 
 var app = express.createServer(express.logger());
+var introBuf = fs.readFileSync('index.html');             // returns a buffer
+var introString = introBuf.toString();                    // default is 'utf8' encoding, and converting the entire buffer
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(introString);
 });
 
 var port = process.env.PORT || 5000;
